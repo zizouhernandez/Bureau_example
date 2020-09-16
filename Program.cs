f7270a8652a0e16250621901b5ae19b09aecaf2e
Zizou <Zizou@Zizos-MacBook-Air.local>
2020-09-16 13:31:27 +0200

    Örfilad

--- Program.cs
+++ Program.cs
@@ -4,6 +4,8 @@ class Program
 {
     static void Main(string[] args)
     {
+        System.Console.WriteLine(@"Du öppnar din sönderörfilade byrå
+        och funderar på om du ska örfila den igen");
         string contents = "";
 
         System.Console.WriteLine("Öppnar byrålådan...");
